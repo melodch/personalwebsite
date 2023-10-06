fc3ad4b099f137819f3a39d7f12e9e8b466831d1
Melody Chiu <melochiu@amazon.com>
2023-10-06 00:37:14 -0400

    update spinning username with spinning tattoo sign, add play/pause button

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,64 @@
-const $element = $('.neutuis-animation');
-const totalFrames = 70;
-const animationDuration = 3500;
+const $element = $('.tattoo-animation');
+const totalFrames = 163;
+const animationDuration = 5300;
 const timePerFrame = animationDuration / totalFrames;
 let timeWhenLastUpdate;
 let timeFromLastUpdate;
 let frameNumber = 1;
 let direction = 1;
+let active = 1;
 
 // 'step' function will be called each time browser rerender the content
 // we achieve that by passing 'step' as a parameter to the 'requestAnimationFrame' function
 function step(startTime) {
-    // 'startTime' is provided by requestAnimationName function, and we can consider it as current time
-    // first of all we calculate how much time has passed from the last time when frame was update
-    if (!timeWhenLastUpdate) timeWhenLastUpdate = startTime;
-    timeFromLastUpdate = startTime - timeWhenLastUpdate;
+    if (active) {
 
-    // then we check if it is time to update the frame
-    if (timeFromLastUpdate > timePerFrame) {
-        // and update it accordingly
-        $element.attr('src', `images/Logo/Neutuis-${frameNumber}.png`);
-        // reset the last update time
-        timeWhenLastUpdate = startTime;
+        // 'startTime' is provided by requestAnimationName function, and we can consider it as current time
+        // first of all we calculate how much time has passed from the last time when frame was update
+        if (!timeWhenLastUpdate) timeWhenLastUpdate = startTime;
+        timeFromLastUpdate = startTime - timeWhenLastUpdate;
 
-        // update frame number based on direction state
-        if (direction == 1) {
-            frameNumber = frameNumber + 1;
-        }
-        else {
-            frameNumber = frameNumber - 1;
-        }
+        // then we check if it is time to update the frame
+        if (timeFromLastUpdate > timePerFrame) {
+            // and update it accordingly
+            $element.attr('src', `images/Logo/Tattoo-${frameNumber}.png`);
+            // reset the last update time
+            timeWhenLastUpdate = startTime;
 
-        // reset frame if it is the last frame
-        if (frameNumber >= totalFrames) {
-            frameNumber = 1;
-        }
-        if (frameNumber <= 0) {
-            frameNumber = totalFrames;
+            // update frame number based on direction state
+            if (direction == 1) {
+                frameNumber = frameNumber + 1;
+            }
+            else {
+                frameNumber = frameNumber - 1;
+            }
+
+            // reset frame if it is the last frame
+            if (frameNumber >= totalFrames) {
+                frameNumber = 1;
+            }
+            if (frameNumber <= 0) {
+                frameNumber = totalFrames;
+            }
         }
     }
 
     requestAnimationFrame(step);
 }
 
-function reverseNeutuis() {
+function reverseTattoo() {
     direction = (direction + 1) % 2;
+}
+
+function playPauseTattoo() {
+    active = (active + 1) % 2;
 }
 
 // create a set of hidden divs and set their background-image attribute
 // to required images that will force browser to download the images
 $(document).ready(() => {
     for (var i = 1; i < totalFrames + 1; i++) {
-        $('body').append(`<div id="preload-image-${i}" <img class="neutuis-animation" src=\`images/Logo/Neutuis-${frameNumber}.png\`></div>`);
+        $('body').append(`<div id="preload-image-${i}" <img class="tattoo-animation" src=\`images/Logo/Neutuis-${frameNumber}.png\`></div>`);
     }
 });
 
